f4f6655b3c2bb8f6fd2d18e2e58f817f4fa34f54
Aline Morais <alinesantana-2010@hotmail.com>
2026-05-05 22:00:46 +0100

    Iplementar parte 2 do projeto

--- scripts/index.js
+++ scripts/index.js
@@ -24,6 +24,52 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
   },
 ];
+
+// Seleção dos elementos do DOM
+const editButton = document.querySelector(".profile__edit-button");
+const editPopup = document.querySelector("#edit-popup");
+const closeButton = editPopup.querySelector(".popup__close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const nameInput = editPopup.querySelector(".popup__input_type_name");
+const descriptionInput = editPopup.querySelector(".popup__input_type_description");
+const formElement = document.querySelector("#edit-profile-form");
+
+// Funções reutilizáveis de abrir e fechar modal
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+
+// Preenche os campos do formulário com os valores atuais do perfil
+function fillProfileForm() {
+  nameInput.value = profileTitle.textContent;
+  descriptionInput.value = profileDescription.textContent;
+}
+
+// Abre o modal e preenche o formulário
+function handleOpenEditModal() {
+  fillProfileForm();
+  openModal(editPopup);
+}
+
+// Lida com o envio do formulário
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
+  closeModal(editPopup);
+}
+
+// Event listeners
+editButton.addEventListener("click", handleOpenEditModal);
+closeButton.addEventListener("click", function () {
+  closeModal(editPopup);
+});
+formElement.addEventListener("submit", handleProfileFormSubmit);
  
 initialCards.forEach((card) => {
   console.log(card.name);
